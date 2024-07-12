5ba2cd42c5c99211e97ec14aa0e56f605635df6c
Éverton M. Vieira <emuvi@outlook.com.br>
2024-07-11 22:30:13 -0300

    feat: small upgrades

--- src/main/java/br/com/pointel/goorv/dektop/pieces/GText.java
+++ src/main/java/br/com/pointel/goorv/dektop/pieces/GText.java
@@ -1,6 +1,12 @@
 package br.com.pointel.goorv.dektop.pieces;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
@@ -70,7 +76,23 @@ public class GText extends JTextArea implements GWays<GText> {
     }
 
     public GText putAct(ActionListener action) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER && (e.getModifiersEx() & InputEvent.CTRL_DOWN_MASK) != 0) {
+                    action.actionPerformed(new ActionEvent(e.getSource(), e.getID(), "CTRL+ENTER"));
+                }
+            }
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 2) {
+                    action.actionPerformed(new ActionEvent(evt.getSource(), evt.getID(), "DOUBLE_CLICK"));
+                }
+            }
+        });
+        return this;
     }
 
 }
